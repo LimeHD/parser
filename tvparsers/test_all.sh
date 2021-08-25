fe519346b70deec17f6dbae2360275af5475af6e
zikwall <andrey.kapitonov.96@gmail.com>
2021-08-25 10:17:36 +0300

    Change test shell script

--- tvparsers/test_all.sh
+++ tvparsers/test_all.sh
@@ -7,8 +7,5 @@ for filename in $(pwd)/*.py; do
 
   echo "Execute $file output to ./tmp/all/$file.csv"
 
-  python3 $file --output $(pwd)/tmp/all/$file.csv
-
-  #sql=$(<$filename)
-  #docker run --rm --network="clickhouse-net" --link clickhouse-$1:clickhouse-server yandex/clickhouse-client -m --host clickhouse-server --query="$sql"
+  docker run -v $(pwd)/tmp/all:/out --rm -it tv_parsers $file --output /out/$file.csv
 done
